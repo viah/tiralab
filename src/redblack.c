20499ad1262d67b8d7565e65f65a66ce37c3cf9f
vahlgren <vahlgren@metso.it.helsinki.fi>
2012-11-07 12:23:08 +0200

    save

--- src/redblack.c
+++ src/redblack.c
@@ -15,15 +15,25 @@ void insert_redblack(char *key, Match *match) {
 
 	RBnode *rbn;
 
+	/* create new node */
 	rbn = malloc(sizeof(RBnode));
 		
 	if(rbn == NULL) fail("insert_redblack(), malloc() failed");
 
 	rbn->key = key;
 	rbn->match = match;
-	rbn->color = BLACK; /* xxx todo */
 	rbn->left = NULL;
 	rbn->right = NULL;
+
+	/* insert the new node in the tree */
+
+	if(root == NULL) {
+		rbn->color = BLACK;
+		root = rbn;
+		return;
+	} 
+
+	
 
 	fail("not implemented\n");
 }
